b0751ed8f6bc7572cf9e62338ee8ef644dfd8b3a
AgusAboitiz <aboitizagustin@gmail.com>
2026-07-22 00:12:19 +0000

    Seed inicial de la tabla series

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO series (serie_id, nombre, tipo, periodicidad, fuente_default) VALUES
+('dolar_oficial', 'Dólar Oficial', 'cotizacion', 'diaria', 'BCRA'),
+('dolar_mep', 'Dólar MEP', 'cotizacion', 'diaria', 'ArgentinaDatos'),
+('uva', 'UVA', 'indice', 'diaria', 'BCRA'),
+('inflacion_mensual', 'Inflación Mensual (IPC)', 'indice', 'mensual', 'ArgentinaDatos');
